9695c67f0e24b8f979f4889642bb12b5db626e86
bnyaminaltinok <bnyaminaltinok@gmail.com>
2021-07-07 00:21:14 +0300

    ok

--- minikube.sh
+++ minikube.sh
@@ -2,5 +2,5 @@
 username=$(whoami)
 base64 C://Users//$username//.minikube/ca.crt > cacrt.txt | tr -d '\n' > cacrt.txt
 base64 C://Users//$username//.minikube//profiles//minikube//client.crt > client.txt  | tr -d '\n' > client.txt
-base64 C://Users//$username//.minikube//profiles//minikube//client.key > clientkey.txt  | tr -d '\n' > clientkey.txtletsgooooo
+base64 C://Users//$username//.minikube//profiles//minikube//client.key > clientkey.txt  | tr -d '\n' > clientkey.txt
 cp C://Users//$username//.kube/config .
